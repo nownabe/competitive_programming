8cf63cef799cab4dc9cfe9e141ceaa0298296773
nownabe <nownabe@gmail.com>
2021-04-11 22:39:59 +0900

    ABC198A Div

---
+++ AtCoder/abc198a.rb
@@ -0,0 +1,18 @@
+# https://atcoder.jp/contests/abc198/tasks/abc198_a
+
+class A
+  def initialize
+  end
+
+  def run
+    n = gets.to_i
+
+    puts solve(n)
+  end
+
+  def solve(n)
+    n - 1
+  end
+end
+
+A.new.run
