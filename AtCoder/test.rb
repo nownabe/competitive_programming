88832ff81bb2342766e8f8aa1473090ffb863fcc
nownabe <nownabe@gmail.com>
2021-04-19 20:36:33 +0900

    Update test.rb

--- AtCoder/test.rb
+++ AtCoder/test.rb
@@ -11,25 +11,29 @@ samples = code.split("__END__\n")[1].split("=input\n")[1..-1]
 samples.each_with_index do |sample, i|
   input, expect = sample.split("=expect\n")
 
-  stdin, stdout, _, thread = *Open3.popen3("ruby #{filename}")
+  stdin, stdout, stderr, thread = *Open3.popen3("ruby #{filename}")
   stdin.write input
   actual = stdout.read
 
   thread.join
 
   puts
-  if actual == expect
+  if actual == expect && thread.value.success?
     puts "Sample #{i} - OK"
   else
     puts "Sample #{i} - Failed"
     puts
-    puts "    input:"
-    input.each_line { |l| puts "        #{l}" }
-    puts
-    puts "    expect:"
-    expect.each_line { |l| puts "        #{l}" }
-    puts
-    puts "    actual:"
-    actual.each_line { |l| puts "        #{l}" }
+    if thread.value.success?
+      puts "    input:"
+      input.each_line { |l| puts "        #{l}" }
+      puts
+      puts "    expect:"
+      expect.each_line { |l| puts "        #{l}" }
+      puts
+      puts "    actual:"
+      actual.each_line { |l| puts "        #{l}" }
+    else
+      puts stderr.read
+    end
   end
 end
