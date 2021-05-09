06e05168d4c3b60012504e4a16581f409bd91851
nownabe <nownabe@gmail.com>
2021-05-09 20:14:27 +0900

    ABC029A 複数形

---
+++ AtCoder/abc029a.rb
@@ -0,0 +1,28 @@
+# https://atcoder.jp/contests/abc029/tasks/abc029_a
+
+class ABC029A
+  attr_reader
+
+  def initialize
+    @w = gets.chomp
+  end
+
+  def run
+    puts "#{@w}s"
+  end
+
+  def solve
+  end
+end
+
+ABC029A.new.run
+
+__END__
+=input
+dog
+=expect
+dogs
+=input
+chokudai
+=expect
+chokudais
