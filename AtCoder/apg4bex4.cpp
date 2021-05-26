02104df24c9fb650e071799ffd51df3c88c47c30
nownabe <nownabe@users.noreply.github.com>
2021-05-26 20:04:10 +0900

    APG4bEX4 ○年は何秒？

---
+++ AtCoder/apg4bex4.cpp
@@ -0,0 +1,12 @@
+// https://atcoder.jp/contests/apg4b/tasks/APG4b_cs
+
+#include <iostream>
+
+int main() {
+  int seconds = 365 * 24 * 60 * 60;
+
+  std::cout << seconds << std::endl;
+  std::cout << 2 * seconds << std::endl;
+  std::cout << 5 * seconds << std::endl;
+  std::cout << 10 * seconds << std::endl;
+}
